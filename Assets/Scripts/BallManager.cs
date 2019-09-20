8b2a891e19554ad26188648a58afa757c93f75ae
ひなるひ <hop-step-turningrestart@outlook.jp>
2019-09-21 00:26:07 +0900

    とりあえずボールが動き出せた

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour
+{
+
+    //GameManagerからgetcomponentで弾の状況を受け取る。
+    public GameObject gamemaster;
+
+    private Vector3 velocity = new Vector3(0, 0, 0);
+
+
+
+    //定数群
+    private const float throwspeed = 10.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void ThrowInit()
+    {
+        Rigidbody rd = this.GetComponent<Rigidbody>();
+
+        this.velocity = new Vector3(0, 0, throwspeed);
+        rd.useGravity = true;
+    }
+
+    
+    //ここからはボールの状況によって処理が変更されます。
+    public void Throwing()
+    {
+        this.transform.position += velocity;
+
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //ボールの状態
+    public enum BallStatus
+    {
+        //待機中
+        waiting,
+        //投げられた
+        throwed,
+        //当たるか当たらないかの瀬戸際の時
+        dodging,
+        //バットから逃れた
+        avoided,
+        //打たれて飛ばされているとき
+        flying
+    };
+    BallStatus ballstatus;
+
+
+    GameObject ballObj;
+
+
+    Camera mainCamera;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballObj = GameObject.Find("Ball");
+
+        mainCamera = Camera.main;
+
+
+
+        ballstatus = BallStatus.waiting;
+
+        mainCamera.transform.position = new Vector3(0, 10, -10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (ballstatus)
+        {
+            case BallStatus.waiting:
+                if (Input.GetButtonDown("Throw"))
+                {
+                    ballstatus = BallStatus.throwed;
+                    ballObj.GetComponent<BallManager>().ThrowInit();
+                    
+                }
+                break;
+            case BallStatus.throwed:
+                ballObj.GetComponent<BallManager>().Throwing();
+                break;
+
+            default:
+                Debug.Log("You are an idiot!");
+                break;
+        }
+    }
+}
